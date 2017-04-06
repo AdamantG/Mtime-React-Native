36b78996c4c09301c587cb7dde504cc87b0efded
LMJ_Company <447180005@qq.com>
2017-04-06 16:55:05 +0800

    ListView替换FlatList中...

--- app/src/screen/MovieShowScreen.js
+++ app/src/screen/MovieShowScreen.js
@@ -5,20 +5,17 @@
  */
 
 import React, {Component} from "react";
-import {ListView} from "react-native";
+import {Animated, FlatList, ListView} from "react-native";
 import ShowItem from "../component/ShowItem";
 
 export default class TabRecentScreen extends Component {
+    AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
     constructor(props) {
         super(props);
 
         this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (r1, r2) => {
-                    r1 !== r2;
-                }
-            })
+            dataSource: [],
         };
         this.fetchShowMovies = this.fetchShowMovies.bind(this);
 
@@ -33,32 +30,26 @@ export default class TabRecentScreen extends Component {
             .then((response) => response.json())
             .then((responseData) => {
                 // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+                const data = responseData.ms;
+                for (let i = 0; i < data.length; i++) {
+                    let key = 'item';
+                    data[i].key = key.append(i);
+                }
                 this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(responseData.ms),
+                    dataSource: data,
                 });
             });
     }
 
-    // async fetchShowMovies() {
-    //     try {
-    //         // 注意这里的await语句，其所在的函数必须有async关键字声明
-    //         let response = await fetch('https://api-m.mtime.cn/Showtime/LocationMovies.api?locationId=290');
-    //         let responseJson = await response.json();
-    //         console.log(responseJson.toString());
-    //         this.setState({
-    //             dataSource: this.state.dataSource.cloneWithRows(responseJson.ms)
-    //         });
-    //     } catch(error) {
-    //         console.error(error);
-    //     }
-    // }
-
-
     render() {
         return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={(rowData) => <ShowItem movie={rowData}/>}
+            <FlatList
+                data={this.state.dataSource}
+                renderItem={(item, index) => <ShowItem movie={item}/>}
+                getItemLayout={(data, index) => (
+                    // 120 是被渲染 item 的高度 ITEM_HEIGHT。
+                    {length: 143, offset: 143 * index, index}
+                )}
             />
         );
     }
