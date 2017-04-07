0095bbdf56f78b3310dc5bc9a838de3e3bd829a2
LMJ_Company <447180005@qq.com>
2017-04-07 16:26:49 +0800

    添加列表的Footer、Separator

---
+++ app/src/component/ItemSeparator.js
@@ -0,0 +1,23 @@
+/**
+ * author: liminjie
+ * date: 2017/4/7
+ * desc:列表分隔线
+ */
+
+import React, {Component} from "react";
+import {StyleSheet, View} from "react-native";
+
+const SEPARATOR_HEIGHT = StyleSheet.hairlineWidth;
+
+export default class ItemSeparator extends Component {
+    render() {
+        return <View style={styles.separator}/>;
+    }
+}
+
+const styles = StyleSheet.create({
+    separator: {
+        height: 1,
+        backgroundColor: 'gray',
+    },
+});
---
+++ app/src/component/ListFooter.js
@@ -0,0 +1,23 @@
+/**
+ * author: liminjie
+ * date: 2017/4/7
+ * desc:列表底部Footer
+ */
+
+import React, {Component} from "react";
+import {StyleSheet, Text} from "react-native";
+
+export default class ListFooter extends Component {
+    render() {
+        return <Text style={styles.text}>人家是有底线的</Text>;
+    }
+}
+
+const styles = StyleSheet.create({
+    text: {
+        flex: 1,
+        fontSize: 12,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
