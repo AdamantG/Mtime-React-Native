f31432c16dd04317e209d1e87025d12478b12f7a
452MJ <447180005@qq.com>
2017-05-06 20:52:42 +0800

    尝试Video属性的使用

--- app/src/screen/VideoPlayScreen.js
+++ app/src/screen/VideoPlayScreen.js
@@ -7,17 +7,28 @@
 'use strict';
 
 import React, {Component} from "react";
-import {Image, Text, TouchableOpacity, View, Animated, FlatList} from "react-native";
+import {TouchableOpacity, View, StyleSheet, Text} from "react-native";
 import {NavigationActions} from "react-navigation";
-import {styles} from "../style/Styles";
-import VideoItem from "../component/VideoItem";
-import ItemSeparator from "../component/ItemSeparator";
-import ListFooter from "../component/ListFooter";
 import Video from "react-native-video";
 
 export default class VideoPlayScreen extends Component {
 
+    state = {
+        fullScreen: false,
+        volume: 1,
+        muted: false,
+        resizeMode: 'contain',
+        duration: 0.0,
+        currentTime: 0.0,
+        paused: true,
+    };
+
+    video: Video;
+
     render() {
+        const flexCompleted = this.getCurrentTimePercentage() * 100;
+        const flexRemaining = (1 - this.getCurrentTimePercentage()) * 100;
+
         const navigation = this.props.navigation;
         const params = navigation.state.params;
         const video = params.video;
@@ -25,25 +36,139 @@ export default class VideoPlayScreen extends Component {
         const hightUrl = video.hightUrl;
 
         return (
-            <View style={{
-                flex: 1
-            }}>
-                <Video source={{uri: url}}   // Can be a URL or a local file.
-                       resizeMode="contain"                      // Fill the whole screen at aspect ratio.*
-                       repeat={true}                           // Repeat forever.
-                       playInBackground={false}
-                       style={{
-                           position: 'absolute',
-                           top: 0,
-                           left: 0,
-                           bottom: 0,
-                           right: 0,
-                       }}
-                />
+            <View style={styles.container}>
+                <TouchableOpacity
+                    style={styles.fullScreen}
+                    onPress={() => this.setState({paused: !this.state.paused})}
+                >
+                    <Video
+                        ref={(ref: Video) => {
+                            this.video = ref
+                        }}
+                        /* For ExoPlayer */
+                        source={{uri: url}}
+                        style={styles.fullScreen}
+                        rate={this.state.rate}
+                        paused={this.state.paused}
+                        volume={this.state.volume}
+                        muted={this.state.muted}
+                        resizeMode={this.state.resizeMode}
+                        onLoad={this.onLoad}
+                        onProgress={this.onProgress}
+                        onEnd={this.onEnd}
+                        onAudioBecomingNoisy={this.onAudioBecomingNoisy}
+                        onAudioFocusChanged={this.onAudioFocusChanged}
+                        repeat={false}
+                    />
+                </TouchableOpacity>
 
+                <View style={styles.controls}>
+                    <View style={styles.generalControls}>
+                        <View style={styles.rateControl}>
+                            <TouchableOpacity onPress={() => {
+                                if (this.state.fullScreen) {
+                                    this.video.dismissFullscreenPlayer();
+                                } else {
+                                    this.video.presentFullscreenPlayer();
+                                }
+                            }}>
+                                <Text style={[styles.controlOption,]}>全屏</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.volumeControl}>
+                            {this.renderVolumeControl(0.5)}
+                            {this.renderVolumeControl(1)}
+                            {this.renderVolumeControl(1.5)}
+                        </View>
+
+                        <View style={styles.resizeModeControl}>
+                            {this.renderResizeModeControl('cover')}
+                            {this.renderResizeModeControl('contain')}
+                            {this.renderResizeModeControl('stretch')}
+                        </View>
+                    </View>
+
+                    <View style={styles.trackingControls}>
+                        <View style={styles.progress}>
+                            <View style={[styles.innerProgressCompleted, {flex: flexCompleted}]}/>
+                            <View style={[styles.innerProgressRemaining, {flex: flexRemaining}]}/>
+                        </View>
+                    </View>
+                </View>
             </View>
-
         );
+    }
+
+    onLoad = (data) => {
+        this.setState({duration: data.duration});
+    };
+
+    onProgress = (data) => {
+        this.setState({currentTime: data.currentTime});
+    };
+
+    onEnd = () => {
+        this.setState({paused: true})
+        this.video.seek(0)
+    };
+
+    onAudioBecomingNoisy = () => {
+        this.setState({paused: true})
+    };
+
+    onAudioFocusChanged = (event: { hasAudioFocus: boolean }) => {
+        this.setState({paused: !event.hasAudioFocus})
+    };
+
+    getCurrentTimePercentage() {
+        if (this.state.currentTime > 0) {
+            return parseFloat(this.state.currentTime) / parseFloat(this.state.duration);
+        }
+        return 0;
+    };
+
+    renderRateControl(rate) {
+        const isSelected = (this.state.rate === rate);
+
+        return (
+            <TouchableOpacity onPress={() => {
+                this.video.presentFullscreenPlayer();
+                this.setState({rate})
+            }}>
+                <Text style={[styles.controlOption, {fontWeight: isSelected ? 'bold' : 'normal'}]}>
+                    {rate}x
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+
+    renderResizeModeControl(resizeMode) {
+        const isSelected = (this.state.resizeMode === resizeMode);
+
+        return (
+            <TouchableOpacity onPress={() => {
+                this.setState({resizeMode})
+            }}>
+                <Text style={[styles.controlOption, {fontWeight: isSelected ? 'bold' : 'normal'}]}>
+                    {resizeMode}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+
+    renderVolumeControl(volume) {
+        const isSelected = (this.state.volume === volume);
+
+        return (
+            <TouchableOpacity onPress={() => {
+                this.setState({volume})
+            }}>
+                <Text style={[styles.controlOption, {fontWeight: isSelected ? 'bold' : 'normal'}]}>
+                    {volume * 100}%
+                </Text>
+            </TouchableOpacity>
+        )
     }
 
     _onPressBack = () => {
@@ -52,3 +177,72 @@ export default class VideoPlayScreen extends Component {
         navigation.dispatch(NavigationActions.back());
     };
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+    },
+    fullScreen: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+    controls: {
+        backgroundColor: 'transparent',
+        borderRadius: 5,
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+    },
+    progress: {
+        flex: 1,
+        flexDirection: 'row',
+        borderRadius: 3,
+        overflow: 'hidden',
+    },
+    innerProgressCompleted: {
+        height: 20,
+        backgroundColor: '#cccccc',
+    },
+    innerProgressRemaining: {
+        height: 20,
+        backgroundColor: '#2C2C2C',
+    },
+    generalControls: {
+        flex: 1,
+        flexDirection: 'row',
+        borderRadius: 4,
+        overflow: 'hidden',
+        paddingBottom: 10,
+    },
+    rateControl: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    volumeControl: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    resizeModeControl: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    controlOption: {
+        alignSelf: 'center',
+        fontSize: 11,
+        color: 'white',
+        paddingLeft: 2,
+        paddingRight: 2,
+        lineHeight: 12,
+    },
+});
