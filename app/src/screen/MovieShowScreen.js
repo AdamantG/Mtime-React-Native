e9be55bf7f634719a700f35304e1ea51b1ad6ac4
LMJ_Company <447180005@qq.com>
2017-05-16 14:27:38 +0800

    修改locationId为365（广州）

--- app/src/screen/MovieShowScreen.js
+++ app/src/screen/MovieShowScreen.js
@@ -29,7 +29,7 @@ export default class MovieShowScreen extends Component {
         this.setState({
             refresh: true,
         });
-        fetch('https://api-m.mtime.cn/Showtime/LocationMovies.api?locationId=290')
+        fetch('https://api-m.mtime.cn/Showtime/LocationMovies.api?locationId=365')
             .then((response) => response.json())
             .then((responseData) => {
                 // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
